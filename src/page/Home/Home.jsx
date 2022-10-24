b7f458e77cefc6315176be4a9df0df34f0750045
Ibraim Barrios <barrios.party@hotmail.com>
2022-10-23 19:29:16 -0500

    feat: traverse the list

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -3,6 +3,17 @@ import React, { useState } from 'react'
 function Home() {
     const [data, setData] = useState(0);
 
+    const array = [
+        {
+            id: 1,
+            name: 'ibraim'
+        },
+        {
+            id: 2, 
+            name: 'Jose'
+        }
+    ]
+
 const handleClick = () => {
     setData(10); 
 };
@@ -12,6 +23,13 @@ const handleClick = () => {
         <div>Este es mi home</div>
         <span>{data}</span>
         <button onClick={handleClick}>Transformamos A</button>
+
+        <span>Name list</span>
+        <ul>
+            {array.map(item => (
+                <li key={item.id}>{item.name}</li>
+            ))}
+        </ul>
     </>
   )
 }
